7e88ce172f55c774a3b22c9c243c4b675e22f27b
blake-swl <seulwoolee93@gmail.com>
2019-01-28 23:30:26 -0800

    [edit] build

--- public/src/components/Location/index.jsx
+++ public/src/components/Location/index.jsx
@@ -12,17 +12,25 @@ const Location = () => (
           <p>(424) 361-5001</p>
           <p>M-Sat 11:30a - 9:30p</p>
           <p>Sun 12p - 7p</p>
+          <div className="location-buttons">
+            <a href="https://ordering.chownow.com/order/9755/locations?add_cn_ordering_class=true" target="_blank" className="location-order">Order Now</a>
+            <a href="https://www.google.com/maps/place/2536+Overland+Ave,+Los+Angeles,+CA+90064/@34.0400858,-118.4251053,17z/data=!3m1!4b1!4m5!3m4!1s0x80c2bbbc249a8c07:0x730ca2992c109169!8m2!3d34.0400814!4d-118.4229166" target="_blank" className="location-directions">Get Directions</a>
+          </div>
         </div>
       </div>
       <div className="location-item culver">
         <div className="location-overlay">
-            <h3>Culver City</h3>
-            <p>2536 Overland Ave.</p>
-            <p>Los Angeles, CA 90064</p>
-            <p>(424) 361-5001</p>
-            <p>M-Sat 11:30a - 9:30p</p>
-            <p>Sun 12p - 7p</p>
+          <h3>Culver City</h3>
+          <p>2536 Overland Ave.</p>
+          <p>Los Angeles, CA 90064</p>
+          <p>(424) 361-5001</p>
+          <p>M-Sat 11:30a - 9:30p</p>
+          <p>Sun 12p - 7p</p>
+          <div className="location-buttons">
+            <a href="" className="location-order">Order Now</a>
+            <a href="" className="location-directions">Get Directions</a>
           </div>
+        </div>
       </div>
     </div>
   </div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
 
 module.exports = {
   entry: path.resolve('./src'),
   output: {
-    path: path.resolve('./dist'),
+    path: path.resolve('./build'),
     filename: 'bundle.js'
   },
   module: {
@@ -33,9 +36,24 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx']
   },
-  watchOptions: {
-    poll: true,
-    ignored: /node_modules/
+  mode: 'production',
+  plugins: [
+    // new Dotenv({
+    //   path: './.env'
+    // }),
+    // new webpack.DefinePlugin({
+    //   DEBUG: process.env.NODE_ENV !== 'production',
+    //       'process.env': {
+    //           'NODE_ENV': JSON.stringify(process.env.NODE_ENV || "development"),
+    //           'API_KEY': JSON.stringify(process.env.API_KEY)}}),
+    new HtmlWebpackPlugin({
+      // injects bundle.js to our new index.html
+      inject: true,
+      // copys the content of the existing index.html to the new /build index.html
+      template:  path.resolve('./dist/index.html'),
+    }),
+  ],
+  optimization: {
+    minimize: true
   },
-  mode: 'development'
 }
