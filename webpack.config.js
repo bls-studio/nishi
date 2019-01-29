194525307635aed0789275644b6f83e8f67fc9d5
blake-swl <seulwoolee93@gmail.com>
2019-01-28 23:50:57 -0800

    [edit] editing webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,9 @@ const webpack = require('webpack');
 
 
 module.exports = {
-  entry: path.resolve('./public/dist/index.html'),
+  entry: path.resolve('./src/index.js'),
   output: {
-    path: path.resolve('../build'),
+    path: path.resolve('./build'),
     filename: 'bundle.js'
   },
   module: {
