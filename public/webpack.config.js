19570705da3411744b0f6cb64f06c4b90802389c
blake-swl <seulwoolee93@gmail.com>
2019-01-29 18:15:18 -0800

    [edit] webpack

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -1,12 +1,10 @@
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
-
 
 module.exports = {
   entry: path.resolve('./src'),
   output: {
-    // path: path.resolve('./dist'),
     path: path.resolve('../build'),
     filename: 'bundle.js'
   },
@@ -24,53 +22,17 @@ module.exports = {
         test: [/\.css$/, /\.scss$/],
         loader: ['style-loader', 'css-loader', 'sass-loader']
       },
-      { 
-        test: /\.(png|woff|woff2|eot|ttf|svg|otf)$/, 
-        loader: 'url-loader?limit=100000' 
-      },
+      // { 
+      //   test: /\.(png|woff|woff2|eot|ttf|svg|otf)$/, 
+      //   loader: 'url-loader?limit=100000' 
+      // },
       {
         test: /\.(gif|png|jpe?g|svg|pdf)$/i,
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug: true, // webpack@1.x
-              disable: false, // webpack@2.x and newer
-              mozjpeg: {
-                progressive: true,
-                quality: 75
-              },
-              // optipng.enabled: false will disable optipng
-              optipng: {
-                enabled: false,
-              },
-              pngquant: {
-                quality: '65-90',
-                speed: 4
-              },
-              gifsicle: {
-                interlaced: true,
-                optimizationLevel: 2,
-              },
-            },
-          },
-        ],
+        loader: 'file-loader'
       }
-    ]  },
-  resolve: {
-    extensions: ['.js', '.jsx']
+    ]
   },
-  mode: 'production',
   plugins: [
-    // new Dotenv({
-    //   path: './.env'
-    // }),
-    // new webpack.DefinePlugin({
-    //   DEBUG: process.env.NODE_ENV !== 'production',
-    //       'process.env': {
-    //           'NODE_ENV': JSON.stringify(process.env.NODE_ENV || "development"),
-    //           'API_KEY': JSON.stringify(process.env.API_KEY)}}),
     new HtmlWebpackPlugin({
       // injects bundle.js to our new index.html
       inject: true,
@@ -78,7 +40,12 @@ module.exports = {
       template:  path.resolve('./dist/index.html'),
     }),
   ],
-  optimization: {
-    minimize: true
+  resolve: {
+    extensions: ['.js', '.jsx']
   },
+  watchOptions: {
+    poll: true,
+    ignored: /node_modules/
+  },
+  mode: 'production'
 }
