03288116b64bdeba2784f7b75a7072421018165c
blake-swl <seulwoolee93@gmail.com>
2019-01-29 17:34:11 -0800

    [edit] webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const webpack = require('./public/node_modules/webpack');
 
 
 module.exports = {
-  entry: path.resolve('./src/index.js'),
+  entry: path.resolve('./src'),
   output: {
     // path: path.resolve('./dist'),
     path: path.resolve('../build'),
@@ -24,16 +24,40 @@ module.exports = {
         test: [/\.css$/, /\.scss$/],
         loader: ['style-loader', 'css-loader', 'sass-loader']
       },
-      // { 
-      //   test: /\.(png|woff|woff2|eot|ttf|svg|otf)$/, 
-      //   loader: 'url-loader?limit=100000' 
-      // },
+      { 
+        test: /\.(png|woff|woff2|eot|ttf|svg|otf)$/, 
+        loader: 'url-loader?limit=100000' 
+      },
       {
         test: /\.(gif|png|jpe?g|svg|pdf)$/i,
-        loader: 'file-loader'
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: false, // webpack@2.x and newer
+              mozjpeg: {
+                progressive: true,
+                quality: 75
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: '65-90',
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: true,
+                optimizationLevel: 2,
+              },
+            },
+          },
+        ],
       }
-    ]
-  },
+    ]  },
   resolve: {
     extensions: ['.js', '.jsx']
   },
