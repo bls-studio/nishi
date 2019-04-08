a849a467e510f6c62a4af85016a123cecde2d416
blake-swl <seulwoolee93@gmail.com>
2019-04-08 16:53:48 -0700

    [fix] store hours

--- public/src/components/Location/index.jsx
+++ public/src/components/Location/index.jsx
@@ -10,8 +10,8 @@ const Location = () => (
           <p>2536 Overland Ave.</p>
           <p>Los Angeles, CA 90064</p>
           <p>(424) 361-5001</p>
-          <p>M-Sat 11:30a - 9:30p</p>
-          <p>Sun 12p - 7p</p>
+          <p>M-Sat 11:30a - 9:00p</p>
+          <p>Sun 11:30p - 8:00p</p>
           <div className="location-buttons">
             <a href="https://ordering.chownow.com/order/9755/locations?add_cn_ordering_class=true" target="_blank" className="location-order" rel="external noreferrer">Order Now</a>
             <a href="https://maps.google.com/?q=2536 Overland Ave, Los Angeles, CA 90064" target="_blank" className="location-directions" rel="external noreferrer">Get Directions</a>
@@ -24,8 +24,8 @@ const Location = () => (
           <p>4263 Overland Ave.</p>
           <p>Culver City, CA 90230</p>
           <p>(424) 361-5001</p>
-          <p>M-Sat 11:30a - 9:30p</p>
-          <p>Sun 12p - 7p</p>
+          <p>M-Sat 11:30a - 9:00p</p>
+          <p>Sun 12:00p - 7:30p</p>
           <div className="location-buttons">
             <a href="https://ordering.chownow.com/order/9755/locations?add_cn_ordering_class=true" target="_blank" className="location-order" rel="external noreferrer">Order Now</a>
             <a href="https://maps.google.com/?q=4263 Overland Ave, Culver City, CA 90230" className="location-directions" rel="external noreferrer">Get Directions</a>
