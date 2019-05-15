051a9dd68381c8d484f9d26cce1d206396870bd4
blake-swl <seulwoolee93@gmail.com>
2019-05-15 15:12:16 -0700

    [edit] added coming soon screen

--- public/src/components/App/index.jsx
+++ public/src/components/App/index.jsx
@@ -6,16 +6,18 @@ import { Route, Switch  } from 'react-router-dom';
 import Left from '../DigitalMenu/LeftScreen';
 import Center from '../DigitalMenu/CenterScreen';
 import Right from '../DigitalMenu/RightScreen';
-// import Digital from '../DigitalMenu';
 import Home from './switch.jsx';
+import Soon from '../ComingSoon';
 
 const App = () => (
   <div id="outer-container" className="parallax">
+    {/* <Soon /> */}
     <Switch>
       <Route exact path="/" component={Home}/>
       <Route exact path="/right" component={Left}/>
       <Route exact path="/center" component={Center}/>
       <Route exact path="/left" component={Right}/>
+      <Route exact path="/soon" component={Soon} />
     </Switch>
   </div>
 );
