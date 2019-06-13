8e542335ce5410253ac0c87922ba8674febc0f86
blake-swl <seulwoolee93@gmail.com>
2019-06-12 21:38:53 -0700

    [fix]

--- public/src/components/App/index.jsx
+++ public/src/components/App/index.jsx
@@ -7,18 +7,16 @@ import Left from '../DigitalMenu/LeftScreen';
 import Center from '../DigitalMenu/CenterScreen';
 import Right from '../DigitalMenu/RightScreen';
 import Home from './switch.jsx';
-import Soon from '../ComingSoon';
 
 const App = () => (
   <div id="outer-container" className="parallax">
-    <Center />
-    {/* <Switch>
+
+    <Switch>
       <Route exact path="/" component={Home}/>
       <Route exact path="/right" component={Left}/>
       <Route exact path="/center" component={Center}/>
       <Route exact path="/left" component={Right}/>
-      <Route exact path="/soon" component={Soon} />
-    </Switch> */}
+    </Switch>
   </div>
 );
 {/* <Menu /> */}
