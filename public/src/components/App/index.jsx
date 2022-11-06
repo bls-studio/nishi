80cf4d22eee30af9929c411e8fa006d8e4f04954
blake-swl <seulwoolee93@gmail.com>
2022-11-05 21:15:48 -0700

    fix

--- public/src/components/App/index.jsx
+++ public/src/components/App/index.jsx
@@ -6,7 +6,6 @@ import { Route, Switch  } from 'react-router-dom';
 import Left from '../DigitalMenu/LeftScreen';
 import Center from '../DigitalMenu/CenterScreen';
 import Right from '../DigitalMenu/RightScreen';
-import Home from './switch.jsx';
 
 const App = () => (
   <div id="outer-container" className="parallax">
@@ -14,7 +13,6 @@ const App = () => (
     <Left />
     <Center /> */}
     <Switch>
-      <Route exact path="/" component={Home}/>
       <Route exact path="/right" component={Left}/>
       <Route exact path="/center" component={Center}/>
       <Route exact path="/left" component={Right}/>
